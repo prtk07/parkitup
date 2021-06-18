c628375a4d7b0db02bec77fb39a66ba91283cade
Prateek Verma <prateekverma70@gmail.com>
2021-06-18 06:12:19 +0530

    feat: authentication enabled via LoginPage

--- src/Views/LoginPage.js
+++ src/Views/LoginPage.js
@@ -2,27 +2,88 @@ import React, { useState } from "react";
 import "./LoginPage.css";
 
 function LoginPage() {
-  const [login, setLogin] = useState(true);
+  const [loginpage, setLoginpage] = useState(true);
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [mobile, setMobile] = useState("");
   function toggleLogin() {
-    setLogin(!login);
+    setLoginpage(!loginpage);
+  }
+
+  function Signup(a, b, c, d) {
+    // console.log(a, b, c, d);
+    fetch("http://localhost:8080/api/signup", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: a,
+        password: b,
+        name: c,
+        mobile: d,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message) alert(data.message);
+        else {
+          console.log(data);
+          localStorage.setItem("token", data.token);
+          window.location.replace("/");
+        }
+      })
+      .catch((e) => {
+        alert("Server Not Responding");
+      });
+  }
+
+  function Login(userName, pass) {
+    fetch("http://localhost:8080/api/login", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: userName,
+        password: pass,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message) alert(data.message);
+        else {
+          console.log(data);
+          localStorage.setItem("token", data.token);
+          window.location.replace("/");
+        }
+      })
+      .catch((e) => {
+        alert("Server Not Responding");
+      });
   }
 
   return (
     <div className="login-container">
-      {login ? (
+      {loginpage ? (
         <div className="login">
           <h1>Login</h1>
           <div className="inputs">
-            {/* <p>Username</p> */}
-            <input placeholder="Email" className="input-box" type="text" />
-            {/* <p>Password</p> */}
+            <input placeholder="Username" className="input-box" type="text" />
             <input
               placeholder="Password"
               className="input-box"
               type="password"
             />
           </div>
-          <button>Login</button>
+          <button
+            onClick={() => {
+              Login(username, password);
+            }}
+          >
+            Login
+          </button>
           <div className="signup-link-container">
             <p>Not registered yet?</p>
             <p className="signup-link" onClick={toggleLogin}>
@@ -34,17 +95,47 @@ function LoginPage() {
         <div className="login" style={{ padding: "0px" }}>
           <h1>Signup</h1>
           <div className="inputs">
-            <input placeholder="Name" className="input-box" type="text" />
-            <input placeholder="Email" className="input-box" type="text" />
+            <input
+              placeholder="Name"
+              className="input-box"
+              type="text"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
+            <input
+              placeholder="username"
+              className="input-box"
+              type="text"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
             <input
               placeholder="Password"
               className="input-box"
               type="password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
             />
 
-            <input placeholder="Mobile" className="input-box" type="text" />
+            <input
+              placeholder="Mobile"
+              className="input-box"
+              type="text"
+              onChange={(e) => {
+                setMobile(e.target.value);
+              }}
+            />
           </div>
-          <button>Signup</button>
+          <button
+            onClick={() => {
+              Signup(username, password, name, mobile);
+            }}
+          >
+            Signup
+          </button>
           <div className="signup-link-container">
             <p>Already a User?</p>
             <p className="signup-link" onClick={toggleLogin}>
